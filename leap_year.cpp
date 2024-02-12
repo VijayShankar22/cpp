8741667cb75cdcd7d3beec5f15bb74f251d8e2d0
Vijay Shankar <vijayshankar1035@gmail.com>
2024-02-12 15:18:19 +0530

    leap year

---
+++ leap_year.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isLeapYear(int year) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    int year;
+    cout << "==============================="<< endl;
+    cout << "C++ PROGRAM TO CHECK LEAP YEAR" << endl;
+    cout << "==============================="<< endl;
+    cout << "Enter year : ";
+    cin >> year;
+
+    if (isLeapYear(year)) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+
+    return 0;
+}
